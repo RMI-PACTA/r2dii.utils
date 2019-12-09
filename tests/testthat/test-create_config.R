f80298ac422550b7b29ee5e3dfef4d6fbdde86d1
Mauro Lepore <maurolepore@gmail.com>
2019-12-09 07:17:07 -0600

    create_config() test is now more robust & focused

--- tests/testthat/test-create_config.R
+++ tests/testthat/test-create_config.R
@@ -1,11 +1,8 @@
 test_that("create_config creates the expected configuration file", {
-  expect_output(
-    out <- create_config("
-     default:
-       field: value
-    "),
-    "Saved"
-  )
+  out <- create_config("
+   default:
+     field: value
+  ")
 
   expect_true(fs::file_exists(out))
   expect_equal(get_param("field")(out), "value")
