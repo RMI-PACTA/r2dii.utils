34ba362cd39dc7b2f73a78c684a42bc7e9dadaf7
Mauro Lepore <maurolepore@gmail.com>
2020-04-20 14:15:23 -0500

    Fix R CMD check warning

--- tests/testthat/test-r2dii_packages.R
+++ tests/testthat/test-r2dii_packages.R
@@ -1,4 +1,7 @@
 test_that("r2dii_packages returns the expected vector", {
+  uses_r2dii <- any(grepl("^r2dii$", rownames(installed.packages())))
+  if (!uses_r2dii) skip("r2dii is not installed.")
+
   expect_equal(
     r2dii_packages(),
     sort(c(
