af37dfeddad41ecac989d53df51067aef7ff3102
Mauro Lepore <maurolepore@gmail.com>
2020-04-20 13:27:15 -0500

    Assert r2dii is installed

--- R/r2dii_packages.R
+++ R/r2dii_packages.R
@@ -6,8 +6,21 @@
 #' @family miscellaneous utility functions
 #'
 #' @examples
-#' r2dii_packages()
+#' # The r2dii package must be installed
+#' # See https://2degreesinvesting.github.io/r2dii/#installation
+#' if (requireNamespace("r2dii")) {
+#'   r2dii_packages()
+#' }
 r2dii_packages <- function() {
+  if (!requireNamespace("r2dii")) {
+    abort(
+      glue(
+        "The r2dii package must be installed. Install it from GitHub with:
+         # install.packages('remotes')
+         remotes::install_github('2degreesinvesting/r2dii')"
+      )
+    )
+  }
   pkgs <- utils::packageDescription("r2dii", fields = "Depends", drop = T)
   pkgs <- strsplit(pkgs, ",")[[1]]
   vapply(pkgs, function(x) sub("\n", "", x), character(1), USE.NAMES = FALSE)
